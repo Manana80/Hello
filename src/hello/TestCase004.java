2bff2b877e90601bdfa84604e664d67729e34dcb
Manana80 <93110108+Manana80@users.noreply.github.com>
2022-01-24 11:54:47 -0500

    TestCase004 conflict has been created

--- src/hello/TestCase004.java
+++ src/hello/TestCase004.java
@@ -1,5 +1,8 @@
 package hello;
 
 public class TestCase004 {
-
+ // this is unkonw changes to feature branch
+    // that exists in master,
+    // now git will not know what to do with this
+    // when we merge and conflict will happen
 }
